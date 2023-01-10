955bb1a1b9506c07b934e79a0bb3def6c6f89f45
Laura <lmcsay@gmail.com>
2023-01-10 16:59:53 -0600

    Create RouterInput and RouterOutput types

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,7 @@
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '@/server/routers';
+import { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 
 function getBaseUrl() {
   if (typeof window !== 'undefined')
@@ -28,3 +29,6 @@ export const trpc = createTRPCNext<AppRouter>({
   },
   ssr: false,
 });
+
+export type RouterInput = inferRouterInputs<AppRouter>;
+export type RouterOutput = inferRouterOutputs<AppRouter>;
