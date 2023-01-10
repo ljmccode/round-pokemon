0640033cd2382eaac2387ab9f3c36fc5d9ab59b4
Laura <lmcsay@gmail.com>
2023-01-10 15:35:27 -0600

    Create fallback pokemon src image to fix TypeScript error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import { getOptionsForVote } from '@/utils/getRandomPokemon'
 export default function Home() {
   const [ids, updateIds] = useState(getOptionsForVote);
   const [first, second] = ids;
+  const fallback = "https://w7.pngwing.com/pngs/282/481/png-transparent-pokemon-pokeball-illustration-pikachu-ash-ketchum-pokemon-pokeball-pokemon-johto-technology.png"
 
   const firstPokemon = trpc.getPokemonById.useQuery({ id: first})
   const secondPokemon = trpc.getPokemonById.useQuery({ id: second})
@@ -21,12 +22,12 @@ export default function Home() {
       <div className='p-2'></div>
       <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
         <div className='w-64 h-64 flex flex-col'>
-          <img  src={firstPokemon.data?.sprites.front_default} alt={firstPokemon.data?.name} className='w-full'/>
+          <img  src={firstPokemon.data?.sprites.front_default ?? fallback} alt={firstPokemon.data?.name} className='w-full'/>
           <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
         </div>
         <div className='p-8'>Vs</div>
         <div className='w-64 h-64 flex flex-col'>
-          <img  src={secondPokemon.data?.sprites.front_default} alt={secondPokemon.data?.name} className='w-full'/>
+          <img  src={secondPokemon.data?.sprites.front_default ?? fallback} alt={secondPokemon.data?.name} className='w-full'/>
           <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
         </div> 
         <div className='p-2'></div>
