9ef6f2f6f31677b0c7810d2ac0f3a58605ca4dcd
Laura <lmcsay@gmail.com>
2023-01-12 12:20:01 -0600

    Add loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { trpc } from '@/utils/trpc'
 import { getOptionsForVote } from '@/utils/getRandomPokemon'
 import PokemonListing from '../components/PokemonListing'
@@ -26,19 +27,26 @@ export default function Home() {
     updateIds(getOptionsForVote)
   }
 
+  const dataLoaded = !firstPokemon.isLoading && firstPokemon.data && !secondPokemon.isLoading && secondPokemon.data
+  
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className='text-2xl text-center'>Which Pokemon is rounder?</div>
+    <div className="h-screen w-screen flex flex-col justify-between items-center relative">
+      <div className='text-2xl text-center pt-8'>Which Pokemon is rounder?</div>
       <div className='p-2'></div>
-      <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
-        {!firstPokemon.isLoading && firstPokemon.data && !secondPokemon.isLoading && secondPokemon.data && (
-          <>
+        {dataLoaded && (
+          <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
           <PokemonListing pokemon={firstPokemon.data} vote={() => voteForRoundest(first)} />
           <div className='p-8'>Vs</div>
           <PokemonListing pokemon={secondPokemon.data} vote={() => voteForRoundest(second)} />
-          </>
+          <div className='p-2' />
+          </div>
+          
         )}
-        <div className='p-2'></div>
+      {!dataLoaded && (<img className="w-48" src="/rings.svg" alt="loader"/>)}
+      <div className="w-full text-xl text-center pt-4">
+        <Link href="/results">
+          Results
+        </Link>
       </div>
     </div>
   )
